75828a14af08629d40fe97e61e297b3bc3612578
Williams <williams.422024010@civitas.ukrida.ac.id>
2025-06-11 21:39:59 +0700

    API book initial

---
+++ database/migrations/2025_06_11_135401_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('title');
+            $table->string('author', 100);
+            $table->string('publisher', 100);
+            $table->string('publication_year', 4);
+            $table->text('cover')->nullable();
+            $table->text('description')->nullable();
+            $table->double('price')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->integer('created_by')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->integer('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
